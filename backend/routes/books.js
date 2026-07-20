d51eeef42badd90c72eea72e4ec9757c598e4db4
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-20 23:01:27 +0530

    added major routes

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -23,8 +23,10 @@ router.post("/", (req, res) => {
 			message: "All fields are required.",
 		});
 	}
+	const lastBook = books[books.length - 1];
+
 	const newBook = {
-		id: books.length + 1,
+		id: lastBook ? lastBook.id + 1 : 1,
 		title,
 		author,
 		genre,
@@ -33,6 +35,50 @@ router.post("/", (req, res) => {
 
 	books.push(newBook);
 	res.status(201).json(newBook);
+});
+
+router.put("/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const book = books.find((book) => book.id === id);
+
+	if (!book) {
+		return res.status(404).json({
+			message: "book not found",
+		});
+	}
+
+	if (!title || !author || !genre || price === undefined) {
+		return res.status(404).json({
+			message: "Enter all fields",
+		});
+	}
+
+	const { title, author, genre, price } = req.body;
+
+	book.title = title;
+	book.author = author;
+	book.genre = genre;
+	book.price = price;
+
+	res.status(200).json(book);
+});
+
+router.delete("/:id", (req, res) => {
+	const id = Number(req.params.id);
+
+	const index = books.findIndex((book) => book.id === id);
+
+	if (index === -1) {
+		return res.status(404).json({
+			message: "Book not found",
+		});
+	}
+
+	books.splice(index, 1);
+
+	res.status(200).json({
+		message: "Book deleted successfully",
+	});
 });
 
 module.exports = router;
