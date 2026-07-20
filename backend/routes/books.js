dc4940bbdcffd86327e0981de3fdbb48f5aa31cf
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-20 22:22:54 +0530

    id route added

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const books = require("../data/books");
 
 router.get("/", (req, res) => {
-	res.send("Books Page");
+	res.status(200).json(books);
+});
+
+router.get("/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const book = books.find((book) => book.id === id);
+
+	if (!book) return res.status(404).json({ message: "Book not found" });
+
+	res.status(200).json(book);
 });
 
 module.exports = router;
