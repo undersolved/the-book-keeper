a92a3aba3f1afb211edf23d86101fe59cd121d6a
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-20 21:53:12 +0530

    books router added

---
+++ backend/data/books.js
@@ -0,0 +1,25 @@
+const books = [
+	{
+		id: 1,
+		title: "Atomic Habits",
+		author: "James Clear",
+		genre: "Self Help",
+		price: 499,
+	},
+	{
+		id: 2,
+		title: "Harry Potter",
+		author: "J. K. Rowling",
+		genre: "Fantasy",
+		price: 699,
+	},
+	{
+		id: 3,
+		title: "The Alchemist",
+		author: "Paulo Coelho",
+		genre: "Fiction",
+		price: 399,
+	},
+];
+
+module.exports = books;
---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const booksRouter = require("./routes/books");
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+	res.send("Welcome to the Homepage of the bookstore API");
+});
+
+app.use("/books", booksRouter);
+
+app.listen(PORT, () => {
+	console.log(`Server running on Port ${PORT}`);
+});
